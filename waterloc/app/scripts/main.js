5cb8cc1bb3d68e9274e28e69e1a75dcf139fc089
d2fong <d2fong@uwaterloo.ca>
2015-07-13 15:06:30 -0400

    Queries UW data api to get the location of DC, sets the coordinates of DC to be the center point of the google map

--- waterloc/app/scripts/main.js
+++ waterloc/app/scripts/main.js
@@ -1,23 +1,22 @@
 'use strict';
 
-var map;
-var initialize = function() {
-  var mapCanvas = document.getElementById('map-canvas');
-  map = new google.maps.Map(mapCanvas, {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
-  });
-};
-
-google.maps.event.addDomListener(window, 'load', initialize);
 
 
-(function(Models, Views, Services) {
+(function(Models, Views, Services, Secrets) {
   $(document).ready(function() {
+    var uwAPI = new ServiceModule.UWaterlooService(SecretsModule.uwDataKey);
 
+    var map;
+    var map_init = function(dcLocation) {
 
-    var uwAPI = new ServiceModule.UWaterlooService();
-    console.log(uwAPI);
-    //uwAPI.queryBuildings(alert);
+      var mapCanvas = document.getElementById('map-canvas');
+      map = new google.maps.Map(mapCanvas, {
+        zoom: 15,
+        center: {lat: dcLocation.data['latitude'], lng: dcLocation.data['longitude']}
+      });
+    };
+
+    var dc = uwAPI.getBuilding('DC', map_init);
+
   });
-})(ModelModule, ViewModule, ServiceModule);
+})(ModelModule, ViewModule, ServiceModule, SecretsModule);
--- waterloc/app/scripts/services.js
+++ waterloc/app/scripts/services.js
@@ -5,25 +5,62 @@
 
 var ServiceModule = (function() {
 
-  var UWaterlooService = function() {
-    this.key = '';
-    this.urlPrefix = 'https://api.uwaterloo.ca/v2';
+  var cache = {};
+
+  var UWaterlooService = function(apiKey) {
+    this.apiKey = apiKey;
+    this.urlPrefix = 'https://api.uwaterloo.ca/v2/';
+  };
+
+  var getBuildingListUrl =  function(prefix, key) {
+    return prefix + 'buildings/list.json' + '?key=' + key;
+  };
+  var getBuildingCodeUrl =  function(prefix, code, key) {
+    return prefix + 'buildings/' + code + '.json' + '?key=' + key;
   };
 
 
   _.extend(UWaterlooService.prototype, {
-    queryBuildings: function() {
+    queryBuildings: function(callback) {
+      if ("building_list" in cache) {
+        return cache['building_list'];
+      } else {
+        $.ajax({
+          method: "GET",
+          url: getBuildingListUrl(this.urlPrefix,this.apiKey)
+        }).success(function(response) {
+          cache.building_list = response['data'];
+          callback(response);
+          return response['data'];
+        }).error(function() {
+          return undefined;
+        });
+      }
 
     },
 
-    getBuilding: function(buildingCode) {
-
+    getBuilding: function(buildingCode, callback) {
+      if (buildingCode in cache) {
+        return cache[buildingCode];
+      } else {
+        $.ajax({
+          method: "GET",
+          url: getBuildingCodeUrl(this.urlPrefix, buildingCode, this.apiKey)
+        }).success(function(response) {
+          cache.buildingCode = response['data'];
+          callback(response);
+          return response;
+        }).error(function() {
+          console.log("ERROR");
+          return undefined;
+        });
+      }
     }
   });
 
 
   return {
-    UWaterlooService: UWaterlooService,
+    UWaterlooService: UWaterlooService
   };
 
 })();
