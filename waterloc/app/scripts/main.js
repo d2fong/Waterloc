5ab19e85f764ba8187bea6b0e65d61fe60cab334
d2fong <d2fong@uwaterloo.ca>
2015-07-13 13:48:24 -0400

    Home page now spanws a google map that covers the whole window view port

--- waterloc/app/scripts/main.js
+++ waterloc/app/scripts/main.js
@@ -1,7 +1,23 @@
 'use strict';
 
+var map;
+var initialize = function() {
+  var mapCanvas = document.getElementById('map-canvas');
+  map = new google.maps.Map(mapCanvas, {
+    zoom: 8,
+    center: {lat: -34.397, lng: 150.644}
+  });
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
 (function(Models, Views, Services) {
   $(document).ready(function() {
 
+
+    var uwAPI = new ServiceModule.UWaterlooService();
+    console.log(uwAPI);
+    //uwAPI.queryBuildings(alert);
   });
 })(ModelModule, ViewModule, ServiceModule);
